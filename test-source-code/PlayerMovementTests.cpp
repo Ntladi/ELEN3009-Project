43710342f015c7c6d07f808ceda7d6782cb9959e
Chokoet <1705890@students.wits.ac.za>
2019-08-28 19:07:03 +0200

    Implemented player movement tests

---
+++ test-source-code/PlayerMovementTests.cpp
@@ -0,0 +1,184 @@
+#include "Player.h"
+#include <algorithm>
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest.h>
+
+TEST_CASE("A player can be created in the correct orientation")
+{
+	SUBCASE("The player is facing up")
+	{
+		auto player = Player
+		{ Orientation::FACE_UP };
+		auto orientation = player.getOrientation();
+		CHECK(orientation == Orientation::FACE_UP)
+		;
+	}
+
+	SUBCASE("Player is facing down")
+	{
+		auto player = Player
+		{ Orientation::FACE_DOWN };
+		auto orientation = player.getOrientation();
+		CHECK(orientation == Orientation::FACE_DOWN)
+		;
+	}
+
+}
+
+TEST_CASE("A player can be created with the correct size")
+{
+	SUBCASE("The player is facing up")
+	{
+		auto player = Player
+		{ Orientation::FACE_UP };
+		auto[x_size,y_size] = player.getSize();
+
+		auto x_size_ref = player.getParameters().getXLength();
+		auto y_size_ref = player.getParameters().getYHeight();
+		CHECK(x_size == x_size_ref)
+		;
+		CHECK(y_size == y_size_ref)
+		;
+	}
+	SUBCASE("The player is facing down")
+	{
+		auto player = Player
+		{ Orientation::FACE_DOWN };
+		auto[x_size,y_size] = player.getSize();
+
+		auto x_size_ref = player.getParameters().getXLength();
+		auto y_size_ref = player.getParameters().getYHeight();
+		CHECK(x_size == x_size_ref)
+		;
+		CHECK(y_size == y_size_ref)
+		;
+	}
+
+}
+
+TEST_CASE("A player can be created with the correct position")
+{
+	auto x_position_ref = Constants::PLAYER_INITAL_X_POSITION;
+
+	SUBCASE("The player is facing up")
+	{
+		auto player = Player
+		{ Orientation::FACE_UP };
+		auto [x_position,y_position] = player.getPosition();
+		auto y_position_ref = player.getParameters().getScreenYHeight()
+				- player.getParameters().getYHeight() / 2;
+
+		CHECK(x_position == x_position_ref)
+		;
+		CHECK(y_position == y_position_ref)
+		;
+	}
+
+	SUBCASE("The player is facing down")
+	{
+		auto player = Player
+		{ Orientation::FACE_DOWN };
+		auto [x_position,y_position] = player.getPosition();
+		auto y_position_ref = player.getParameters().getYHeight() / 2;
+
+		CHECK(x_position == x_position_ref)
+		;
+		CHECK(y_position == y_position_ref)
+		;
+	}
+}
+
+TEST_CASE("A player on the screen can move left")
+{
+	SUBCASE("The player is facing down")
+	{
+		auto player = Player
+		{ Orientation::FACE_DOWN };
+		auto[x_initial_position, y_initial_position] = player.getPosition();
+		player.setMoveDirection(MoveDirection::LEFT);
+		player.move();
+		auto[x_final_position, y_final_position] = player.getPosition();
+
+		SUBCASE("The player is not in the same position")
+		{
+			CHECK_FALSE(x_initial_position == x_final_position)
+			;
+		}
+
+		SUBCASE("The player has not moved vertically")
+		{
+			CHECK(y_initial_position == y_final_position)
+			;
+		}
+
+		SUBCASE("The player has moved by the correct increment")
+		{
+			auto increment = x_initial_position
+					- player.getParameters().getMovementStep();
+			CHECK(x_final_position == increment)
+			;
+		}
+
+	}
+	SUBCASE("The player is facing up")
+	{
+		auto player = Player
+		{ Orientation::FACE_UP };
+		auto[x_initial_position, y_initial_position] = player.getPosition();
+		player.setMoveDirection(MoveDirection::LEFT);
+		player.move();
+		auto[x_final_position, y_final_position] = player.getPosition();
+		auto increment = x_initial_position
+				- player.getParameters().getMovementStep();
+
+		CHECK_FALSE(x_initial_position == x_final_position)
+		;
+		CHECK(y_initial_position == y_final_position)
+		;
+		CHECK(x_final_position == increment)
+		;
+	}
+}
+
+TEST_CASE("A player can move right")
+{
+	SUBCASE("The player is facing down")
+	{
+		auto player = Player
+		{ Orientation::FACE_DOWN };
+		auto[x_initial_position, y_initial_position] = player.getPosition();
+		player.setMoveDirection(MoveDirection::RIGHT);
+		player.move();
+		auto[x_final_position, y_final_position] = player.getPosition();
+		auto increment = x_initial_position
+				+ player.getParameters().getMovementStep();
+
+		CHECK_FALSE(x_initial_position == x_final_position)
+		;
+		CHECK(y_initial_position == y_final_position)
+		;
+		CHECK(x_final_position == increment)
+		;
+
+	}
+	SUBCASE("The player is facing up")
+	{
+		auto player = Player
+		{ Orientation::FACE_UP };
+		auto[x_initial_position, y_initial_position] = player.getPosition();
+		player.setMoveDirection(MoveDirection::RIGHT);
+		player.move();
+		auto[x_final_position, y_final_position] = player.getPosition();
+		auto increment = x_initial_position
+				+ player.getParameters().getMovementStep();
+
+		CHECK_FALSE(x_initial_position == x_final_position)
+		;
+		CHECK(y_initial_position == y_final_position)
+		;
+		CHECK(x_final_position == increment)
+		;
+	}
+
+}
