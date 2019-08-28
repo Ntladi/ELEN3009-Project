95d2102dd57c375eb1e37dff1e8883993dc058b3
Ntladi <ntladimohajane@gmail.com>
2019-08-28 19:27:31 +0200

    Added definitions for player boundary checking

---
+++ game-source-code/HitBox.h
@@ -0,0 +1,32 @@
+
+#ifndef HITBOX_H_
+#define HITBOX_H_
+#include <tuple>
+#include <vector>
+using two_floats = std::tuple<float, float>;
+
+class HitBox {
+public:
+	HitBox(two_floats position = {}, two_floats size = {});
+	void setNewPositions(two_floats position, two_floats size);
+	std::vector<two_floats> getVertecies(two_floats position, two_floats size);
+	two_floats getTopLeft() const;
+	two_floats getTopRight() const;
+	two_floats getBottomLeft() const;
+	two_floats getBottomRight() const;
+
+private:
+	double x_centre_;
+	double y_centre_;
+	double x_length_from_centre_;
+	double y_length_from_centre_;
+	two_floats top_left_;
+	two_floats top_right_;
+	two_floats bottom_left_;
+	two_floats bottom_right_;
+	void calculateVertecies();
+
+
+};
+
+#endif
--- game-source-code/Player.h
+++ game-source-code/Player.h
@@ -7,6 +7,7 @@
 #include "Orientation.h"
 #include "MoveDirection.h"
 #include "Parameters.h"
+#include "HitBox.h"
 using two_floats = std::tuple<float, float>;
 
 class Player
@@ -17,14 +18,18 @@ public:
 	two_floats getPosition() const;
 	two_floats getSize() const;
 	MoveDirection getMoveDirection() const;
+	HitBox getHitBox() const;
 	Parameters getParameters() const;
 	void setMoveDirection(const MoveDirection &direction);
 	void move();
 
 private:
 	Parameters parameters_;
+	HitBox player_hitbox_;
 	void incrementPlayerPosition();
+	bool isWithinScreenBounds();
 	void movePlayerHorizontally();
+	void updateHitBox();
 };
 
 #endif
